c5ca6f5f1f06c5fc584f5252508762130e9ebc9f
Daniel Saidi <daniel.saidi@gmail.com>
2018-08-27 21:50:06 +0200

    Add new config step

--- scripts/config.sh
+++ scripts/config.sh
@@ -7,3 +7,7 @@ printf "[CONFIG] Finder, Show hidden files\n"
 defaults write com.apple.finder AppleShowAllFiles -bool true
 killall Finder -9
 printf "\n"
+
+printf "[CONFIG] Enable tabbing between all controls\n"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+printf "\n"
